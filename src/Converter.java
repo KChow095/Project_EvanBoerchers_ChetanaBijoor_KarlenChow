3cfece63e4062e2c9a20313395a7bd6a3aa4e1fe
Evan Boerchers <evan.boerchers@ucalgary.ca>
2020-09-11 12:56:13 -0600

    F to C function written

--- src/Converter.java
+++ src/Converter.java
@@ -2,8 +2,13 @@ public class Converter {
 //Your names go here:
 /*
 * @Author: Name of the first student
+<<<<<<< HEAD
+* Chetana Bijoor
+* Name of the third student
+=======
 * Name of the second student
 * Karlen Chow
+>>>>>>> fcc828e541f0dfcf7d34e9c59fc67763a1506aac
 *
 */
 	
@@ -14,8 +19,14 @@ return 0;
 }
 
 private double fahrenheitToCelsius(double F){
+<<<<<<< HEAD
  double C = ((F - 32) * 5) / 9;
 return C;
+=======
+ // TODO: The second student will implement this method
+	return (F-32)*(5/9);
+
+>>>>>>> 19640706625d99449e6e345c94e07c08e779c0db
 }
 
 public static void main(String[] args) {
