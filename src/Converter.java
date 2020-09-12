64d796df1ea235c07d2ba1d03739f2ea818d9c30
Evan Boerchers <evan.boerchers@ucalgary.ca>
2020-09-11 19:15:56 -0600

    Added testin in main and formating testing outputs

--- src/Converter.java
+++ src/Converter.java
@@ -1,5 +1,5 @@
 public class Converter {
-	// This the the development branch
+	// This is the development branch
 	//Your names go here:
 	/*
 	 * @Author: Evan Boerchers, Chetana Bijoor, Karlen Chow
@@ -23,11 +23,17 @@ public class Converter {
 		Converter con = new Converter();
 
 // Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
-		System.out.println(con.celsiusToFahrenheit(180));
+		double C = 180;
+		System.out.println(C + " celsius is " + con.celsiusToFahrenheit(C) + " fahrenheit");
 
 // Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
-		System.out.println(con.fahrenheitToCelsius(250));
-
+		double F = 250;
+		System.out.println(F + " fahrenheit is " + con.fahrenheitToCelsius(F) + " celcius");
+		
+// Kilometer to mile test for 30 kilometers.
+		double km = 30;
+		System.out.println(km + " kilometers is " + con.kilometresToMiles(km) + " miles");
+		
 	}
 
 }
